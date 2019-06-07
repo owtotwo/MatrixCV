3db0e4991fe45c5b4e7acc0d8134002cb901a93b
owtotwo <owtotwo@163.com>
2019-06-07 20:53:59 +0800

    index.html done

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,15 +1,4 @@
-var Main = {
-    data() {
-      return {
-        activeIndex: '1',
-        activeIndex2: '1'
-      };
-    },
-    methods: {
-      handleSelect(key, keyPath) {
-        console.log(key, keyPath);
-      }
-    }
-  }
-var Ctor = Vue.extend(Main)
-new Ctor().$mount('#app')
+var app = new Vue({
+    el: '#app',
+    data: {}
+});
